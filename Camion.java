7b69f407ede64c17aa89ad2239a897afa6d62c26
Endika <esanchezba19dw31@ikzubirimanteo.com>
2020-03-12 09:12:35 +0100

    subida de código

---
+++ Camion.java
@@ -0,0 +1,23 @@
+package Concesionario;
+
+public class Camion extends Vehiculo {
+
+	
+	private int carga;
+	private String tipoMercancia;
+	public int getCarga() {
+		return carga;
+	}
+	public void setCarga(int carga) {
+		this.carga = carga;
+	}
+	public String getTipoMercancia() {
+		return tipoMercancia;
+	}
+	public void setTipoMercancia(String tipoMercancia) {
+		this.tipoMercancia = tipoMercancia;
+	}
+	
+	
+	
+}
